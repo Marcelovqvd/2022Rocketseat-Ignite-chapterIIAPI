e7b201371323204b575bd7bf8766ebeee191c4fe
Marcelo <marcelovqvd@gmail.com>
2022-03-22 16:22:38 -0300

    Criando os Use Case de Categoria

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,11 +1,11 @@
-import { ICategoriesRepository } from "../repositories/ICategoriesRepository";
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 interface IRequest {
   name: string;
   description: string;
 }
 
-export class CreateCategoryService {
+class CreateCategoryUseCase {
   constructor(private categoriesRepository: ICategoriesRepository) {}
   execute({ description, name }: IRequest) {
     const categoryAlreadyExists = this.categoriesRepository.findByName(name);
@@ -17,3 +17,5 @@ export class CreateCategoryService {
     this.categoriesRepository.create({ name, description });
   }
 }
+
+export { CreateCategoryUseCase };
