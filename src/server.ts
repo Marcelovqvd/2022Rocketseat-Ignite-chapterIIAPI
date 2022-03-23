10454db2e3e9ec26af8020e4e44def9d8b598ad1
Marcelo <marcelovqvd@gmail.com>
2022-03-23 01:14:13 -0300

    Refatorando as rotas

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,10 @@
 import express from "express";
 
-import { categoriesRoutes } from "./routes/categories.routes";
-import { specificationRoutes } from "./routes/Specifications.routes";
+import { router } from "./routes";
 
 const app = express();
 
 app.use(express.json());
-
-app.use("/categories", categoriesRoutes);
-app.use("/specification", specificationRoutes);
+app.use(router);
 
 app.listen(3333, () => console.log("Running at 3333"));
