9a22dec16ff29026f5e9a61c8e8a0af2d9cb1bc0
Marcelo <marcelovqvd@gmail.com>
2022-07-19 17:43:15 -0300

    wip

---
+++ src/modules/accounts/useCases/authenticateUsers/AuthenticateUserUseCase.ts
@@ -0,0 +1,52 @@
+import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken";
+import { inject, injectable } from "tsyringe";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+interface IRequest {
+  email: string;
+  password: string;
+}
+
+interface IResponse {
+  user: {
+    name: string;
+    email: string;
+  };
+  token: string;
+}
+
+@injectable()
+class AuthenticateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) {}
+
+  async execute({ email, password }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findByEmail(email);
+
+    if (!user) {
+      throw new Error("Email or password incorrect");
+    }
+
+    const passwordMath = await compare(password, user.password);
+
+    if (!passwordMath) {
+      throw new Error("Email or password incorrect");
+    }
+
+    const token = sign({}, "204005063adcc43b6d54ccb944dd826a", {
+      subject: user.id,
+      expiresIn: "1d",
+    });
+
+    return {
+      user,
+      token,
+    };
+  }
+}
+
+export { AuthenticateUserUseCase };
