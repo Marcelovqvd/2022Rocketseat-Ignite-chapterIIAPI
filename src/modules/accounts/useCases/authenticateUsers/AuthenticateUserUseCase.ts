6cdfe187ed729d9332277787a59881fa3d5c738a
Marcelo <marcelovqvd@gmail.com>
2022-07-20 11:38:38 -0300

    aula: Tratamento de exceções

--- src/modules/accounts/useCases/authenticateUsers/AuthenticateUserUseCase.ts
+++ src/modules/accounts/useCases/authenticateUsers/AuthenticateUserUseCase.ts
@@ -2,6 +2,7 @@ import { compare } from "bcryptjs";
 import { sign } from "jsonwebtoken";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
 interface IRequest {
@@ -28,13 +29,13 @@ class AuthenticateUserUseCase {
     const user = await this.usersRepository.findByEmail(email);
 
     if (!user) {
-      throw new Error("Email or password incorrect");
+      throw new AppError("Email or password invalid");
     }
 
     const passwordMath = await compare(password, user.password);
 
     if (!passwordMath) {
-      throw new Error("Email or password incorrect");
+      throw new AppError("Email or password invalid");
     }
 
     const token = sign({}, "204005063adcc43b6d54ccb944dd826a", {
