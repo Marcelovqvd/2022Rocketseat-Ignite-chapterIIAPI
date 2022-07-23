eb66eb010fe0a71f47f7edd405d34fb05e7e7d55
Marcelo <marcelovqvd@gmail.com>
2022-07-23 08:50:26 -0300

    aula: imports da aplicação

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,14 @@
 import "reflect-metadata";
-import express, { NextFunction } from "express";
+import express from "express";
 import swaggerUi from "swagger-ui-express";
 import "express-async-errors";
 
 import "./database";
 
-import "./shared/container";
+import "@shared/container";
 
-import { AppError } from "./errors/AppError";
+import { AppError } from "@errors/AppError";
+
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -16,19 +17,17 @@ const app = express();
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(router);
-app.use(
-  (err: Error, request: Request, response: Response, next: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        message: err.message,
-      });
-    }
-
-    return response.status(500).json({
-      status: "error",
-      message: `Internal server error - ${err.message}`,
+app.use((err: Error, _request: Request, response: Response) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      message: err.message,
     });
   }
-);
+
+  return response.status(500).json({
+    status: "error",
+    message: `Internal server error - ${err.message}`,
+  });
+});
 
 app.listen(3333, () => console.log("Running at 3333"));
