6f1fd2d4b4243a13e55d68513df39993a2ab0c1d
Marcelo <marcelovqvd@gmail.com>
2022-07-29 16:11:55 -0300

    aula: Criando migrations de imagens de carro

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -1,5 +1,6 @@
+import { Car } from "@modules/cars/infra/typeorm/entities/Car";
+
 import { ICreateCarDTO } from "../dtos/ICreateCarDTO";
-import { Car } from "../infra/typeorm/entities/Car";
 
 interface ICarsRepository {
   create(data: ICreateCarDTO): Promise<Car>;
